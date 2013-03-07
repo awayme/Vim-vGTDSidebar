d9b56b0208b9a9494f1fa6c7f24dac6df6f33e1f
Dersu <awayme@gmail.com>
2013-03-07 22:46:03 +0800

    change wave file path

--- autoload/bell.py
+++ autoload/bell.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import sys
 
-soundFile = 'C:/Program Files/CONEXANT/SAII/BlueStream.wav'
+soundFile = 'C:/Program Files (x86)/Workrave/share/sounds/subtle/rest-break-ended.wav'
 
 def playSound():
     if sys.platform[:5] == 'linux':
